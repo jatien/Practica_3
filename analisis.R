46548343a75e58176d98ede03a2239e218e48722
Javi Torres <javierhshshhs@gmail.com>
2023-10-09 17:25:48 +0200

    act

--- analisis.R
+++ analisis.R
@@ -1,3 +1,12 @@
 library("tidyverse")
 data <- read.csv("HousingData.csv")
+print(data, width=50)
 
+#Correlaciones con media de vivienda
+a<-data %>%
+  select(5:7,13,14)
+na.omit
+
+pairs(a,
+      col = c("red", "blue"), 
+      pch = 18)
