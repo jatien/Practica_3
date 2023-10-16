f8caa6ebe282b3873adcbdb5ae4eaccfeca3c251
Javi Torres <javierhshshhs@gmail.com>
2023-10-16 17:55:53 +0200

    act

--- analisis.R
+++ analisis.R
@@ -1,12 +1,28 @@
 library("tidyverse")
+library("ggcorrplot")
+library("GGally")
+
 data <- read.csv("HousingData.csv")
 print(data, width=50)
 
 #Correlaciones con media de vivienda
 a<-data %>%
-  select(5:7,13,14)
-na.omit
+  na.omit
 
-pairs(a,
+a_1 <- data %>%
+  select(6,13,11,14) %>%
+  na.omit
+a %>%
+  select(14,13) %>%
+  na.omit(.) %>% 
+  cor(.) %>% 
+  ggcorrplot(., hc.order = TRUE,
+             type = "lower")
+a_2 = subset(a_1,CHAS == 0)
+
+ggpairs(a_2)
+
+pairs(a_2,
       col = c("red", "blue"), 
       pch = 18)
+
