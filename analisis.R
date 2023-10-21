dac02ad7e97237b593e7fb08543a41b82448fa46
Javi Torres <javierhshshhs@gmail.com>
2023-10-22 00:36:24 +0200

    act

--- analisis.R
+++ analisis.R
@@ -1,28 +1,38 @@
-library("tidyverse")
-library("ggcorrplot")
-library("GGally")
+library(tidyverse)
+library(ggcorrplot)
+library(GGally)
+library(MVA)
 
-data <- read.csv("HousingData.csv")
-print(data, width=50)
 
-#Correlaciones con media de vivienda
-a<-data %>%
+df <- as.data.frame(Boston)
+
+selected_vars <- c("dis", "tax", "ptratio", "black", "lstat", "medv")
+df_subset <- df[, selected_vars]
+
+df_1 <- df %>% 
+  dplyr::select(6,11,13,14) %>%
   na.omit
 
+
+pairs(df_1, col='darkgreen',pch=18)
+
+#dataset para comparar media de precio
 a_1 <- data %>%
-  select(6,13,11,14) %>%
+  dplyr::select(6,11,13,14) %>%
   na.omit
-a %>%
-  select(14,13) %>%
-  na.omit(.) %>% 
+
+#dataset para contaminación
+a_2 <- data %>%
+  dplyr::select(8,7,3,5) %>%
+  na.omit
+
+df %>%
   cor(.) %>% 
-  ggcorrplot(., hc.order = TRUE,
-             type = "lower")
-a_2 = subset(a_1,CHAS == 0)
+  ggcorrplot(., hc.order = TRUE)
+
+pairs(a_1,
+      col = c( "darkgreen"), 
+      pch = 18)
 
 ggpairs(a_2)
-
-pairs(a_2,
-      col = c("red", "blue"), 
-      pch = 18)
 
